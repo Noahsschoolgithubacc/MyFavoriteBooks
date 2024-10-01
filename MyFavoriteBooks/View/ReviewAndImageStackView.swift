39a065db9b0ec99f222e71894f631b8a062fb148
Noah <Nphilli7@students.tulsatech.edu>
2024-10-01 13:38:02 -0500

    ReviewAndImageStackView is done

--- MyFavoriteBooks/View/ReviewAndImageStackView.swift
+++ MyFavoriteBooks/View/ReviewAndImageStackView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ReviewAndImageStackView: View {
-    
+
     @ObservedObject var book: Book
     @Binding var image: Image?
     @State var showImagePicker = false
     @State var showingDialog = false
-    
+
     var body: some View {
         VStack {
             Divider()
@@ -21,6 +21,7 @@ struct ReviewAndImageStackView: View {
             TextField("Review...", text: $book.microReview)
             Divider()
                 .padding(.vertical)
+            Book.Image(image: image, title: book.title, cornerRadius: 10)
             
             HStack {
                 if image != nil {
@@ -33,21 +34,31 @@ struct ReviewAndImageStackView: View {
                 Button("Update Image...") {
                     showImagePicker = true
                 }
-                
+
                 Spacer()
             }
-            
+
             .padding()
-            
+
             Spacer()
         }
-        
-//        .sheet(isPresented: $showImagePicker) {
-//            ImagePicker(image: $image)
-//        }
+
+        .sheet(isPresented: $showImagePicker) {
+            ImagePicker(image: $image)
+        }
+
+        .confirmationDialog(
+            "Delete Image for \(book.title)?",
+            isPresented: $showingDialog
+        ) {
+            Button("Delete", role: .destructive) { image = nil }
+        } message: {
+            Text("Delete image for \(book.title)?")
+        }
     }
 }
 
 #Preview {
     ReviewAndImageStackView(book: .init(), image: .constant(nil))
+        .padding()
 }
